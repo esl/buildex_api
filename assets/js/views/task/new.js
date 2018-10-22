f428270da9aa6feb6c0f071f905f62ab1f131d9e
sescobb27 <s.escob.b.27@gmail.com>
2018-10-22 13:14:18 -0500

    [add] dynamically add ENV vars

--- assets/js/views/task/new.js
+++ assets/js/views/task/new.js
@@ -4,6 +4,11 @@ export default class NewTaskForm {
     if (btnAddCommand) {
       btnAddCommand.addEventListener('click', this.onAddCommand, false)
     }
+
+    const btnAddEnv = document.getElementById('add-env')
+    if (btnAddEnv) {
+      btnAddEnv.addEventListener('click', this.onAddEnv.bind(this), false)
+    }
   }
 
   onAddCommand (event) {
@@ -15,6 +20,42 @@ export default class NewTaskForm {
     input.name = 'task[commands][]'
     input.type = 'text'
     commandGroup.appendChild(input)
+  }
+
+  onAddEnv (event) {
+    event.preventDefault()
+
+    const envGroup = document.querySelector('[data-env-group]')
+    const [newEnvInput] = document.getElementsByName('new-env')
+    const key = newEnvInput.value
+
+    const keyInput = document.createElement('input')
+    keyInput.className = 'form-control'
+    keyInput.name = `task[env][${key}]`
+    keyInput.type = 'text'
+
+    const inputLabel = document.createElement('label')
+    inputLabel.setAttribute('for', keyInput.name)
+    inputLabel.innerHTML = key
+
+    const deleteEnvLink = document.createElement('a')
+    deleteEnvLink.innerHTML = 'X'
+    deleteEnvLink.onclick = this.onDeleteEnv(inputLabel, keyInput)
+
+    envGroup.appendChild(inputLabel)
+    envGroup.appendChild(keyInput)
+    envGroup.appendChild(deleteEnvLink)
+
+    newEnvInput.value = ''
+  }
+
+  onDeleteEnv (inputLabel, keyInput) {
+    return function (event) {
+      event.preventDefault()
+      inputLabel.parentNode.removeChild(inputLabel)
+      keyInput.parentNode.removeChild(keyInput)
+      this.parentNode.removeChild(this)
+    }
   }
 }
 
