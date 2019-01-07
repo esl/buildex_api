4cf8128c2c0fb1df4a62d1c6dd7fbcb1a50a7815
sescobb27 <s.escob.b.27@gmail.com>
2019-01-07 14:34:00 -0500

    [ci skip][change] mix format

--- lib/release_admin/release_tasks/common.ex
+++ lib/release_admin/release_tasks/common.ex
@@ -5,20 +5,20 @@ defmodule ReleaseAdmin.ReleaseTasks.Common do
 
   @spec setup((() -> any())) :: any()
   def setup(fun) do
-      me = :release_admin
+    me = :release_admin
 
-      IO.puts("Loading #{me}..")
-      Application.load(me)
+    IO.puts("Loading #{me}..")
+    Application.load(me)
 
-      IO.puts("Starting dependencies..")
-      Enum.each(@start_apps, &Application.ensure_all_started/1)
+    IO.puts("Starting dependencies..")
+    Enum.each(@start_apps, &Application.ensure_all_started/1)
 
-      IO.puts("Starting repo..")
-      RuntimeConfig.get_repo().start_link(pool_size: 1)
+    IO.puts("Starting repo..")
+    RuntimeConfig.get_repo().start_link(pool_size: 1)
 
-      fun.()
+    fun.()
 
-      IO.puts("Success!")
-      :init.stop()
+    IO.puts("Success!")
+    :init.stop()
   end
 end
