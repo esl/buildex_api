02e90bcabc16e8b22697399b70da2653713f9ddc
sescobb27 <s.escob.b.27@gmail.com>
2019-01-07 11:39:40 -0500

    [add] setup phoenix to work with releases

--- lib/release_admin_web/endpoint.ex
+++ lib/release_admin_web/endpoint.ex
@@ -51,11 +51,6 @@ defmodule ReleaseAdminWeb.Endpoint do
   configuration should be loaded from the system environment.
   """
   def init(_key, config) do
-    if config[:load_from_system_env] do
-      port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
-      {:ok, Keyword.put(config, :http, [:inet6, port: port])}
-    else
-      {:ok, config}
-    end
+    Confex.Resolver.resolve(config)
   end
 end
