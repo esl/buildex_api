df0fd6518e4d7a1f81c5fd0397a9de789e3c9562
bryan <bryan.hunt@erlang-solutions.com>
2019-01-10 23:17:33 +0000

    add postgres port to prod config

--- config/prod.exs
+++ config/prod.exs
@@ -13,10 +13,10 @@ config :logger, level: :info
 
 config :release_admin, :db_secret_key, {:system, :base64, "DB_SECRET_KEY"}
 
-# Configure your database
 config :release_admin, ReleaseAdmin.Repo,
   username: {:system, "POSTGRES_USERNAME"},
   password: {:system, "POSTGRES_PASSWORD"},
   database: "release_admin",
   hostname: {:system, "POSTGRES_HOSTNAME"},
+  port: {:system, :integer, "POSTGRES_PORT", 5432},
   pool_size: 10
