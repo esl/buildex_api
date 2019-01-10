b5aafa461de2d296d287504e9aa97b73f86a7e6c
bryan <bryan.hunt@erlang-solutions.com>
2019-01-10 16:40:49 +0000

    updated configs

--- config/config.exs
+++ config/config.exs
@@ -1,23 +1,14 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
-# General application configuration
 config :release_admin,
-  ecto_repos: [ReleaseAdmin.Repo],
-  runtime_config: ReleaseAdmin.LiveConfig
+  ecto_repos: [ReleaseAdmin.Repo]
 
-# Configures the endpoint
 config :release_admin, ReleaseAdminWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "s2uhDrAvgw8B8Ei9YtQ4ftuxodQBPDSEIenJJR8VakidMlAIybhx5J+1o7lvpXla",
   render_errors: [view: ReleaseAdminWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: ReleaseAdmin.PubSub, adapter: Phoenix.PubSub.PG2]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:user_id]
@@ -29,9 +20,7 @@ config :ueberauth, Ueberauth,
   ]
 
 config :ueberauth, Ueberauth.Strategy.Github.OAuth,
-  client_id: System.get_env("RELEASE_ADMIN_GITHUB_CLIENT_ID"),
-  client_secret: System.get_env("RELEASE_ADMIN_GITHUB_CLIENT_SECRET")
+  client_id: {:system, "GITHUB_CLIENT_ID"},
+  client_secret: {:system, "GITHUB_CLIENT_SECRET"}
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- config/prod.exs
+++ config/prod.exs
@@ -1,20 +1,22 @@
 use Mix.Config
 
 config :release_admin, ReleaseAdminWeb.Endpoint,
-  http: [port: {:system, :integer, "PORT"}],
+  http: [port: {:system, :integer, "PORT", 8080}],
   load_from_system_env: true,
-  url: [host: "localhost", port: {:system, :integer, "PORT"}],
+  url: [host: {:system, "HOST", "localhost"}],
+  port: {:system, :integer, "PORT", 8080},
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :release_admin, :db_secret_key, {:system, :base64, "DB_SECRET_KEY"}
+
 # Configure your database
 config :release_admin, ReleaseAdmin.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: {:system, "RELEASE_ADMIN_POSTGRES_USERNAME"},
-  password: {:system, "RELEASE_ADMIN_POSTGRES_PASSWORD"},
+  username: {:system, "POSTGRES_USERNAME"},
+  password: {:system, "POSTGRES_PASSWORD"},
   database: "release_admin",
-  hostname: {:system, "RELEASE_ADMIN_POSTGRES_HOSTNAME"},
+  hostname: {:system, "POSTGRES_HOSTNAME"},
   pool_size: 10
