28c6e7b07258bf3926d6790f4379dfc9661803af
bryan <bryan.hunt@erlang-solutions.com>
2019-06-06 12:35:42 +0100

    refactor RPC call

--- lib/buildex_api/services/rpc.ex
+++ lib/buildex_api/services/rpc.ex
@@ -6,11 +6,11 @@ defmodule Buildex.API.Services.RPC do
 
   def start_polling_repo(repo) do
     Config.get_poller_domain()
-    |> :rpc.call(RepoPoller.PollerSupervisor, :start_child, [repo])
+    |> :rpc.call(Buildex.Poller.PollerSupervisor, :start_child, [repo])
   end
 
   def stop_polling_repo(url) do
     Config.get_poller_domain()
-    |> :rpc.call(RepoPoller.PollerSupervisor, :stop_child, [url])
+    |> :rpc.call(Buildex.Poller.PollerSupervisor, :stop_child, [url])
   end
 end
